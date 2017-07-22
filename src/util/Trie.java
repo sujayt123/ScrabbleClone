3261c51b5bc3f39a2d51d8e709126c562f6495e6
Sujay Tadwalkar <sujayt123@gmail.com>
2017-07-21 22:45:08 -0500

    Setting up utilities for backend

---
+++ src/util/Trie.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.*;
+
+/**
+ * Created by sujay on 7/21/17.
+ */
+public class Trie {
+
+    private TrieNode root;
+
+    public Trie()
+    {
+        root = new TrieNode(false);
+
+        File file = new File("dictionary.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process the line.
+                root.insertWord(line.toLowerCase(), 0);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/util/TrieNode.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by sujay on 7/21/17.
+ */
+public class TrieNode {
+
+    private Map<Character, TrieNode> outgoingEdges;
+    private boolean isWord;
+
+    public TrieNode(boolean isWord)
+    {
+        this.isWord = isWord;
+        outgoingEdges = new HashMap<>();
+    }
+
+    public Map<Character, TrieNode> getOutgoingEdges() {
+        return outgoingEdges;
+    }
+
+    public boolean isWord() {
+        return isWord;
+    }
+
+    void insertWord(String s, int index) {
+        if (index < s.length())
+        {
+                TrieNode child = outgoingEdges.containsKey(s.charAt(index)) ?
+                        outgoingEdges.get(s.charAt(index)) :
+                        new TrieNode(false);
+                outgoingEdges.put(s.charAt(index), child);
+                child.insertWord(s, index + 1);
+        }
+        else {
+            isWord = true;
+        }
+    }
+}
